f3b137a84dceb46cc0d8c8c34963843a60aa9397
rayhook <koohyar@gmail.com>
2024-05-17 09:16:52 -0300

    query list of decks

--- server/flashcardapp/api.py
+++ server/flashcardapp/api.py
@@ -4,14 +4,14 @@ from flashcardapp.models import Deck
 api = NinjaAPI()
 
 
-# class DesckSchema(Schema):
-#     name: str
+class DesckSchema(Schema):
+    name: str
 
 
 @api.get("/deck")
 def deck(request):
 
     # query list of all decks from database
-    deck_list = Deck.objects.all()
+    deck_list = Deck.objects.all().values()
 
-    return deck_list
+    return {"decks": list(deck_list)}
