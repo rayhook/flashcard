16e652989bc998b67d5712a3347cd6e980772488
rayhook <koohyar@gmail.com>
2024-05-22 11:22:06 -0300

    Create foreign key for card model

--- server/flashcardapp/models.py
+++ server/flashcardapp/models.py
@@ -9,3 +9,6 @@ class Card(models.Model):
     front = models.TextField()
     back = models.TextField()
     is_learned = models.BooleanField(default=False)
+    deck = models.ForeignKey(
+        "flashcardapp.Deck", related_name=("cards"), on_delete=models.CASCADE, null=True
+    )
