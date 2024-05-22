1e09bcda8a87f6cf19c4f57c8c0e390a345bf29b
rayhook <koohyar@gmail.com>
2024-05-22 11:12:40 -0300

    Create Card model

--- server/flashcardapp/models.py
+++ server/flashcardapp/models.py
@@ -3,3 +3,9 @@ from django.db import models
 
 class Deck(models.Model):
     name = models.CharField(max_length=50)
+
+
+class Card(models.Model):
+    front = models.TextField()
+    back = models.TextField()
+    is_learned = models.BooleanField(default=False)
